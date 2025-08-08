855c04f9a457dd38c84934c4dacffef702f681e0
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2025-08-08 20:54:05 +0200

    [2025-08-08 20:54:05]

--- include/bnf.h
+++ include/bnf.h
@@ -1,23 +1,35 @@
 #ifndef BNF_H
     #define BNF_H
 
-    #ifndef BNF_SYMBOL_TERMINAL_OPEN
-        #define BNF_SYMBOL_TERMINAL_OPEN '\''
+    #ifndef BNF_STR_TERMINAL_OPEN
+        #define BNF_STR_TERMINAL_OPEN "'"
     #endif
 
-    #ifndef BNF_SYMBOL_TERMINAL_CLOSE
-        #define BNF_SYMBOL_TERMINAL_CLOSE '\''
+    #ifndef BNF_STR_TERMINAL_CLOSE
+        #define BNF_STR_TERMINAL_CLOSE "'"
     #endif
 
-    #ifndef BNF_SYMBOL_RULE_OPEN
-        #define BNF_SYMBOL_RULE_OPEN '<'
+    #ifndef BNF_STR_REGEX_OPEN
+        #define BNF_STR_REGEX_OPEN "\""
     #endif
 
-    #ifndef BNF_SYMBOL_RULE_CLOSE
-        #define BNF_SYMBOL_RULE_CLOSE '>'
+    #ifndef BNF_STR_REGEX_CLOSE
+        #define BNF_STR_REGEX_CLOSE "\""
+    #endif
+
+    #ifndef BNF_STR_RULE_OPEN
+        #define BNF_STR_RULE_OPEN "<"
+    #endif
+
+    #ifndef BNF_STR_RULE_CLOSE
+        #define BNF_STR_RULE_CLOSE ">"
     #endif
 
     #ifndef BNF_STR_EQUIV
         #define BNF_STR_EQUIV "::="
+    #endif
+
+    #ifndef BNF_STR_LINE_COMMENT
+        #define BNF_STR_LINE_COMMENT ";"
     #endif
 #endif
